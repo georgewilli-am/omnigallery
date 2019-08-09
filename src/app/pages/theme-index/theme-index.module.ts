126fc620f644db3ee8d9bbe9324199efb076648f
George Sumpster <gsumpster@gmail.com>
2019-08-09 15:14:01 -0700

    added app-theme-cards

--- src/app/pages/theme-index/theme-index.module.ts
+++ src/app/pages/theme-index/theme-index.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ThemeIndexComponent } from './theme-index.component';
 import ThemeIndexRouting from './theme-index.routing';
+import { ThemeCardComponent } from 'src/app/components/theme-card/theme-card.component';
 
 @NgModule({
-  declarations: [ThemeIndexComponent],
+  declarations: [ThemeIndexComponent, ThemeCardComponent],
   imports: [CommonModule, ThemeIndexRouting]
 })
 export class ThemeIndexModule {}
